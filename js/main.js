13326f4af6116d0db3ca26792477a90ddec69d62
vanita22 <vanitayrafita2004@gmail.com>
2017-05-22 02:16:46 -0300

    subiendo archivos

--- js/main.js
+++ js/main.js
@@ -1,3 +1,51 @@
 function validateForm(){
 	/* Escribe tú código aquí */
+
+
+var valido = true;
+	if(document.getElementById("name").value == " " ){
+		alert("ingrese nombre válido");
+		return valido = false;
+	}
+
+	if(document.getElementById("name").value != toUppercase([A-Z])){
+		alert("ingrese nombre con mayúsculas válido");
+		return valido = false;
+	}
+
+
+	if(document.getElementById("lastname").value == " "){
+		alert("ingrese apellido válido");
+		return valido = false;
+	}
+
+	if(document.getElementById("lastname").value != toUppercase([A-Z])){
+		alert("ingrese apellido con mayúsculas válido");
+		return valido = false;
+	}
+
+
+	var expresion = /^[a-z][\w.-]+@\w[\w.-]+\.[\w.-]*[a-z][a-z]$/i;
+	var email = document.formulario.input-email.value;
+	if (!expresion.test(input-email)){
+		alert("ingrese email válido");
+        return valido = false;
+	}
+
+	if(document.getElementById('input-password').value < 6){
+		alert("ingrese password válido");
+    	return valido = false;
+	}
+
+	if(document.getElementById('input-password').value =="password" ){
+		alert("ingrese password válido");
+    	return valido = false;
+	}
+
+	if(document.getElementById('input-password').value =="123456" || "098754" ){
+		alert("ingrese password válido");
+    	return valido = false;
+	}
+
+return valido;
 }
